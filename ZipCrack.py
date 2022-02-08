3c741e94170e25d9784ae278e4b8be2b9dcc1fd5
bearhuang <bearhuang@tencent.com>
2022-02-08 17:53:56 +0800

    内存优化

--- ZipCrack.py
+++ ZipCrack.py
@@ -11,23 +11,26 @@ class ZipCracker:
     maxLength = 10
     minLength = 1
     maxThread = 10
-    dictionaries = None
+    __dictionaries = None
     __isCracked = False
     __lengths = None
-    pool = None
+    __pool = None
+    __zfile = None
 
-    def __init__(self, filePath, minLength=1, maxLength=10):
+    def __init__(self, filePath, minLength=1, maxLength=10, maxThread=10):
         self.filePath = filePath
         self.minLength = minLength
         self.maxLength = maxLength
         self.__lengths = range(minLength, maxLength + 1)
+        self.maxThread = maxThread
         # chr(97) -> 'a' 这个变量保存了密码包含的字符集
-        self.dictionaries = [chr(i) for i in
-                             chain(
-                                 range(97, 123),  # a - z
-                                 range(65, 91),  # A - Z
-                                 range(48, 58))]  # 0 - 9
-        self.pool = ThreadPoolExecutor(max_workers=self.maxThread)
+        self.__dictionaries = [chr(i) for i in
+                               chain(
+                                   # range(97, 123),  # a - z
+                                   # range(65, 91),  # A - Z
+                                   range(48, 58))]  # 0 - 9
+        self.__pool = ThreadPoolExecutor(max_workers=self.maxThread)
+        self.__zfile = ZipFile(self.filePath, 'r')  # 很像open
 
     def __all_passwd(self, dictionaries: List[str], maxlen: int):
         # 返回由 dictionaries 中字符组成的所有长度为 maxlen 的字符串
@@ -43,35 +46,24 @@ class ZipCracker:
 
         yield from helper([0] * maxlen, 0, maxlen)
 
-    def __extract(self, zfile: ZipFile, pwd: str) -> bool:
-        # zfile: 一个ZipFile类, pwd: 密码
-        try:
-            zfile.extractall(path='.', pwd=pwd.encode('utf-8'))  # 密码输入错误的时候会报错
-            print(f"Password is: {pwd}")  # 将正确的密码输出到控制台
-            self.__isCracked = True
-            return True
-        except:
-            return False
-
     def __crack(self, pwd: str):
         try:
-            zfile = ZipFile(self.filePath, 'r')  # 很像open
-            zfile.extractall(path='.', pwd=pwd.encode('utf-8'))  # 密码输入错误的时候会报错
+            self.__zfile.extractall(path='.', pwd=pwd.encode('utf-8'))  # 密码输入错误的时候会报错
             print(f"Password is: {pwd}")  # 将正确的密码输出到控制台
             self.__isCracked = True
             return True
         except:
-            # print(f"Password is not : {pwd}")
             return False
 
     def start(self):
         start = time.time()
-        total = sum(len(self.dictionaries) ** k for k in self.__lengths)  # 密码总数
-        range = tqdm(chain.from_iterable(self.__all_passwd(self.dictionaries, maxlen) for maxlen in self.__lengths),total=total)
+        total = sum(len(self.__dictionaries) ** k for k in self.__lengths)  # 密码总数
+        range = tqdm(chain.from_iterable(self.__all_passwd(self.__dictionaries, maxlen) for maxlen in self.__lengths),
+                     total=total)
         for pwd in range:
-            print(pwd)
-            if self.__isCracked == False:  # 记得extract函数返回的是bool类型的哦
-                self.pool.submit(self.__crack, pwd)
+            # print(pwd)
+            if self.__isCracked == False:
+                self.__pool.submit(self.__crack, pwd)
             else:
                 now = time.time()
                 print("timeCost:", now - start)
