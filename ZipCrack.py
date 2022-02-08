7e368d2b8bb3016ca31b7caff00b68b89a561a2c
bearhuang <bearhuang@tencent.com>
2022-02-08 17:47:13 +0800

    可用

--- ZipCrack.py
+++ ZipCrack.py
@@ -3,6 +3,7 @@ from typing import List
 from tqdm import tqdm
 from itertools import chain
 from zipfile import ZipFile
+from concurrent.futures import ThreadPoolExecutor
 
 
 class ZipCracker:
@@ -13,18 +14,20 @@ class ZipCracker:
     dictionaries = None
     __isCracked = False
     __lengths = None
+    pool = None
 
     def __init__(self, filePath, minLength=1, maxLength=10):
         self.filePath = filePath
         self.minLength = minLength
         self.maxLength = maxLength
-        self.__lengths = range(minLength, maxLength+1)
+        self.__lengths = range(minLength, maxLength + 1)
         # chr(97) -> 'a' 这个变量保存了密码包含的字符集
         self.dictionaries = [chr(i) for i in
                              chain(
-                                 # range(97, 123),    # a - z
-                                 # range(65, 91),    # A - Z
+                                 range(97, 123),  # a - z
+                                 range(65, 91),  # A - Z
                                  range(48, 58))]  # 0 - 9
+        self.pool = ThreadPoolExecutor(max_workers=self.maxThread)
 
     def __all_passwd(self, dictionaries: List[str], maxlen: int):
         # 返回由 dictionaries 中字符组成的所有长度为 maxlen 的字符串
@@ -50,15 +53,26 @@ class ZipCracker:
         except:
             return False
 
+    def __crack(self, pwd: str):
+        try:
+            zfile = ZipFile(self.filePath, 'r')  # 很像open
+            zfile.extractall(path='.', pwd=pwd.encode('utf-8'))  # 密码输入错误的时候会报错
+            print(f"Password is: {pwd}")  # 将正确的密码输出到控制台
+            self.__isCracked = True
+            return True
+        except:
+            # print(f"Password is not : {pwd}")
+            return False
+
     def start(self):
         start = time.time()
-        zfile = ZipFile(self.filePath, 'r')  # 很像open
         total = sum(len(self.dictionaries) ** k for k in self.__lengths)  # 密码总数
-        for pwd in tqdm(chain.from_iterable(self.__all_passwd(self.dictionaries, maxlen) for maxlen in self.__lengths),
-                        total=total):
-            # print(pwd)
-            if self.__extract(zfile, pwd):  # 记得extract函数返回的是bool类型的哦
+        range = tqdm(chain.from_iterable(self.__all_passwd(self.dictionaries, maxlen) for maxlen in self.__lengths),total=total)
+        for pwd in range:
+            print(pwd)
+            if self.__isCracked == False:  # 记得extract函数返回的是bool类型的哦
+                self.pool.submit(self.__crack, pwd)
+            else:
                 now = time.time()
                 print("timeCost:", now - start)
                 break
-
